2705beebaac5241f77ebbecd285a85dbda07f7e7
IG <iikim2511@gmail.com>
2023-09-19 00:28:30 -0400

    slider

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -4,14 +4,17 @@ import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 import "./Slider.scss";
 
 const Slider = () => {
+  // 🦄
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  // 🦄
   const data = [
     "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
   ];
 
+// 🦄
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
   };
@@ -21,12 +24,22 @@ const Slider = () => {
 
   return (
     <div className="slider">
+
+      {/*
+          0x 100 = 0vw (translate)
+          1x 100 = 100vw (translate)
+          2x 100 = 200vw (translate)
+      */}
       <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+
+      {/* // 🦄 */}
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
       </div>
       <div className="icons">
+
+      {/* // 🦄 */}
         <div className="icon" onClick={prevSlide}>
           <WestOutlinedIcon />
         </div>
